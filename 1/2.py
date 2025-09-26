420ca6510070a6cc4653c0425b7c8d68933d84db
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-09-26 19:39:32 +0300

    cv

--- 1/2.py
+++ 1/2.py
@@ -13,7 +13,7 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 v3 = cv2.imread("img/3.bmp", cv2.IMREAD_ANYDEPTH)
-cv2.namedWindow("OutputPanel", cv2.WINDOW_KEEPRATIO)
+cv2.namedWindow("OutputPanel", cv2.WINDOW_AUTOSIZE)
 cv2.imshow("OutputPanel", v3)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
